c2fdbc386045752ead246553ec31fca3988a04cc
Ishaan Bhardwaj <sashlilac109@gmail.com>
2021-05-03 21:20:20 -0400

    Optimized the interpreter a bit, fixed IO (read comments) The if-chain no longer uses string comparison, it instead uses byte comparison The IO no longer assumes default IO files are correct

--- brainlua.lua
+++ brainlua.lua
@@ -1,8 +1,12 @@
-return function(str, inp)
+return function(str, inp, cell_size)
     if type(str) ~= "string" then
         return
     elseif type(inp) ~= "string" then
         inp = ""
+    end
+
+    if type(cell_size) ~= "number" then
+        cell_size = 256
     end
     
     local tape = {}
@@ -12,44 +16,44 @@ return function(str, inp)
     local inp_ptr = 1
 
     while str_ptr <= #str do
-        local char = str:sub(str_ptr, str_ptr)
-        if char == ">" then
+        local char = str:byte(str_ptr)
+        if char == 62 then
             ptr = ptr + 1
-        elseif char == "<" then
+        elseif char == 60 then
             ptr = ptr - 1
-        elseif char == "+" then
-            tape[ptr] = (tape[ptr] + 1) % 256
-        elseif char == "-" then
-            tape[ptr] = (tape[ptr] - 1) % 256
-        elseif char == "." then
-            io.write(string.char(tape[ptr]))
-            io.flush()
-        elseif char == "," then
+        elseif char == 43 then
+            tape[ptr] = (tape[ptr] + 1) % cell_size
+        elseif char == 45 then
+            tape[ptr] = (tape[ptr] - 1) % cell_size
+        elseif char == 46 then
+            io.stdout:write(string.char(tape[ptr]))
+            io.stdout:flush()
+        elseif char == 44 then
             if #inp <= 0 then
-                tape[ptr] = io.read(1):byte()
+                tape[ptr] = io.stdin:read(1):byte()
             elseif inp_ptr <= #inp then
                 tape[ptr] = inp:byte(inp_ptr)
                 inp_ptr = inp_ptr + 1
             end
-        elseif char == "[" and tape[ptr] == 0 then
+        elseif char == 91 and tape[ptr] == 0 then
             local loop = 1
             while loop > 0 do
                 str_ptr = str_ptr + 1
-                char = str:sub(str_ptr, str_ptr)
-                if char == "[" then
+                char = str:byte(str_ptr)
+                if char == 91 then
                     loop = loop + 1
-                elseif char == "]" then
+                elseif char == 93 then
                     loop = loop - 1
                 end
             end
-        elseif char == "]" and tape[ptr] ~= 0 then
+        elseif char == 93 and tape[ptr] ~= 0 then
             local loop = 1
             while loop > 0 do
                 str_ptr = str_ptr - 1
-                char = str:sub(str_ptr, str_ptr)
-                if char == "[" then
+                char = str:byte(str_ptr)
+                if char == 91 then
                     loop = loop - 1
-                elseif char == "]" then
+                elseif char == 93 then
                     loop = loop + 1
                 end
             end
